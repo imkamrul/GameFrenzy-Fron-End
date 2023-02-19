fd4b4054c8eeba97cca59baff85749b7f6fd0223
kamrul-hasan01 <k17h02@gmail.com>
2023-02-19 20:08:11 +0600

    done

--- pages/_app.js
+++ pages/_app.js
@@ -17,13 +17,13 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="GameVault is your ultimate destination for the latest video games and accessories. Discover the hottest titles across all gaming platforms, including PlayStation, Xbox, Nintendo, and PC. Browse our extensive selection of games, consoles, controllers, and gaming accessories, and get them delivered straight to your doorstep. Shop now and level up your gaming experience with GameVault."
+          content="GameFrenzy is your ultimate destination for the latest video games and accessories. Discover the hottest titles across all gaming platforms, including PlayStation, Xbox, Nintendo, and PC. Browse our extensive selection of games, consoles, controllers, and gaming accessories, and get them delivered straight to your doorstep. Shop now and level up your gaming experience with GameVault."
         />
         <meta
           name="keywords"
           content="video games, gaming, consoles, PlayStation, Xbox, Nintendo, PC games, gaming accessories"
         />
-        <title>GameVault - Your Ultimate Gaming Destination</title>
+        <title>GameFrenzy - Your Ultimate Gaming Destination</title>
       </Head>
 
       {/* <Provider store={store}> */}
